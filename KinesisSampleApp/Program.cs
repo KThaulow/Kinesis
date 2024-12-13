7f8cd4328de521f95d0ff67ae6461f9e050ac714
Kristian Thaulow <kristian1990@gmail.com>
2024-12-13 20:21:18 +0100

    Init

---
+++ KinesisSampleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string bucketName = "thaulow-test-bucket";
+
+        // Create an S3 client
+        var s3Client = new AmazonS3Client(RegionEndpoint.USEast2); // Replace with your region
+
+        try
+        {
+            // Get bucket metadata
+            var response = await s3Client.GetBucketLocationAsync(new GetBucketLocationRequest
+            {
+                BucketName = bucketName
+            });
+
+            Console.WriteLine($"Bucket '{bucketName}' is located in: {response.Location}");
+
+            var objectResult = await s3Client.ListObjectsV2Async(new ListObjectsV2Request
+            {
+                BucketName = bucketName
+            });
+
+            foreach(var s3Object in objectResult.S3Objects)
+            {
+                Console.WriteLine(s3Object.Key);
+            }
+        }
+        catch (AmazonS3Exception ex)
+        {
+            Console.WriteLine($"Error encountered: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unknown error: {ex.Message}");
+        }
+    }
+}
